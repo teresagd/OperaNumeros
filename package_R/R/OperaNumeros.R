f544a0a938f685aeffff97124fd4fe84613b4922
teresagd <gdteresa@gmail.com>
2017-01-05 12:00:05 +0100

    Cargar ficheros
    Cargar ficheros

---
+++ package_R/R/OperaNumeros.R
@@ -0,0 +1,26 @@
+#' @title suma.dos.numeros
+#' @description Funcion para realizar la suma de dos numeros
+#' @param numeros sobre los que se desea obtener el resultado
+#' @return El resultado de la operacion de sumar los numeros elegidos
+#' @examples
+#' suma.dos.numeros(5,5)
+#'
+
+suma.dos.numeros <- function(num1,num2){
+     return (num1+num2)
+}
+
+
+#' @title multiplica.dos.numero
+#' @description Funcion para realizar la multiplicacion de dos dos numeros
+#' @param numeros sobre los que se desea obtener el resultado
+#' @return El resultado de la operacion de multiplicar los numeros elegidos
+#' @examples
+#' multiplica.dos.numeros(4,6)
+#'
+multiplica.dos.numeros <- function(num1,num2){
+  return (num1*num2)
+}
+
+
+
